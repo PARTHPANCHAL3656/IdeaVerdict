09ccb542a97cde99fdf6d9fefeb5854b4db6cdc5
Prafful Sharma <81515755+mellowpraful@users.noreply.github.com>
2026-04-04 15:50:55 +0530

    Wrap routes in ThemeProvider component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { ThemeProvider } from './components/ThemeContext'
 import Landing from './pages/Landing'
 import Auth from './pages/Auth'
 import Dashboard from './pages/Dashboard'
@@ -8,38 +9,40 @@ import AuthGuard from './components/AuthGuard'
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          {/* Public Routes */}
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
 
-        {/* Protected Routes */}
-        <Route
-          path="/dashboard"
-          element={
-            <AuthGuard>
-              <Dashboard />
-            </AuthGuard>
-          }
-        />
-        <Route
-          path="/history"
-          element={
-            <AuthGuard>
-              <History />
-            </AuthGuard>
-          }
-        />
-        <Route
-          path="/results/:id"
-          element={
-            <AuthGuard>
-              <Results />
-            </AuthGuard>
-          }
-        />
-      </Routes>
-    </Router>
+          {/* Protected Routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <AuthGuard>
+                <Dashboard />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/history"
+            element={
+              <AuthGuard>
+                <History />
+              </AuthGuard>
+            }
+          />
+          <Route
+            path="/results/:id"
+            element={
+              <AuthGuard>
+                <Results />
+              </AuthGuard>
+            }
+          />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   )
 }
